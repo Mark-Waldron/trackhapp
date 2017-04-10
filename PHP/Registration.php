b5dcda8abef9ef0adf19e32fe28c9480908f0a79
Mark Waldron <Mark Waldron>
2017-04-10 13:43:12 +0100

    Regisration add

---
+++ PHP/Registration.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Rigistration Page</title>
+</head>
+
+<body>
+	
+	<?php
+		$UserName = $_POST["UserName"];
+		$Password = $_POST["PassWord"];
+		$Firstname = $_POST["FirstName"];
+		$Surename = $_POST["Surename"];
+		$Addressline = $_POST["AddressLine"];
+		$City = $_POST["City"];
+		$Telephone = $_POST["Telephone"];
+		$Mobile = $_POST["Mobile"];
+		
+		$servername = "localhost";
+		$username = "root";
+		$password = "";
+		$dbname = "web_dev_assignment";
+		
+		$conn = new mysqli($servername, $username, $password, $dbname);
+
+		if ($conn->connect_error)
+			{
+			die("Connection failed: " . $conn->connect_error);
+	}
+	
+	$sql = "INSERT INTO Users ( UserName, PassWord, FirstName, Surename, AddressLine, City, Telephone, Mobile)
+			VALUES ('$UserName', '$Password', '$Firstname', '$Surename', '$Addressline', '$City', '$Telephone', '$Mobile')";;
+		
+	if ($conn->query($sql) === TRUE)
+	{
+		echo "Your are now added, you can now sign in";
+		
+		?>
+			<br><a href="http://localhost/WEBD/web_dev_assignment_Frontpage.html">Back to Login</a>
+		<?php	
+	} 
+	else 
+	{
+		echo "Error updating record: " . $conn->error;
+	}
+
+	$conn->close();
+	?>
+
+</body>
+
+</html>
