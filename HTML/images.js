ffa9c17854510f324dde248a0020ec189464e993
catherinegracekane <catherinekane@gmail.com>
2017-04-11 16:16:09 +0100

    layout completely changed again, still working on amending form positions and JS

--- HTML/images.js
+++ HTML/images.js
@@ -15,14 +15,12 @@
     //$('#login_form').show();
 //})
 
-//names of functions as well as attributes should describe them
-function showForm(elementId) { 
-  //now we kick out both conditional we do not need them anymore
+function showLogForm() { 
 
-  //we hide both forms
-  document.getElementById("login_form").style.display="none";
-  document.getElementById("reg_form").style.display="none";
+  document.getElementById("login_form").style.display="block";
+}
 
-  //and then we simply show wanted one isn't that nicer and cleaner?
-  document.getElementById(elementId).style.display="block";
+function showRegForm() { 
+
+  document.getElementById("reg_form").style.display="block";
 }
