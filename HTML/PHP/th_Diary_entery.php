f6ccdc91fdd9276edbe4da1864146fa80aed162d
Mark Waldron <Mark Waldron>
2017-04-11 12:48:44 +0100

    dairy entery added

---
+++ HTML/PHP/th_Diary_entery.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Diary Entery</title>
+</head>
+
+<body>
+	
+	<?php
+		DiaryEntery = $_POST["DiaryEntery"];
+		
+		
+		
+		
+		$servername = "localhost";
+		$username = "root";
+		$password = "";
+		$dbname = "oop-assignment";
+		
+		$conn = new mysqli($servername, $username, $password, $dbname);
+
+		if ($conn->connect_error)
+			{
+			die("Connection failed: " . $conn->connect_error);
+	}
+	
+	$sql = "INSERT INTO Diary_Enterys (DiaryEntery)
+			VALUES ('$DiaryEntery')";;
+		
+	if ($conn->query($sql) === TRUE)
+	{
+		echo "Diary entery added, you can now sign in";
+	} 
+	else 
+	{
+		echo "Error updating record: " . $conn->error;
+	}
+
+	$conn->close();
+	?>
+
+</body>
+
+</html>
