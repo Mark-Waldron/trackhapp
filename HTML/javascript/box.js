708314f3d2b426c3c55d25dd9a15970799289807
Mark Waldron <Mark Waldron>
2017-04-19 01:42:34 +0100

    extreamly rough health added

---
+++ HTML/javascript/box.js
@@ -0,0 +1,23 @@
+function modify_qty(val) {
+    var qty = document.getElementById('qty').value;
+    var new_qty = parseInt(qty,10) + val;
+    
+    if (new_qty < 0) {
+        new_qty = 0;
+    }
+    
+    document.getElementById('qty').value = new_qty + "ml";
+    return new_qty;
+}
+
+function modify_qty_2(val_2) {
+    var qty_2 = document.getElementById('qty_2').value;
+    var new_qty_2 = parseInt(qty_2,10) + val_2;
+    
+    if (new_qty_2 < 0) {
+        new_qty_2 = 0;
+    }
+    
+    document.getElementById('qty_2').value = new_qty_2;
+    return new_qty_2;
+}
