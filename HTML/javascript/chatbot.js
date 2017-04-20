9e43dd014161c2986ab3af6cccadc05940b3ebb5
catherinegracekane <catherinekane@gmail.com>
2017-04-21 00:36:52 +0100

    more amendments to features, change Response on input form and a lot of formatting needed

--- HTML/javascript/chatbot.js
+++ HTML/javascript/chatbot.js
@@ -1,27 +1,41 @@
 function myFunction() {
+    
     document.getElementById("demo").innerHTML = "Test JS File";
     
     var menu_option = 0;
-	var question = '<h1>what is your name?</h1>';				  
+	var question = '<h2>Hello, my name is TrackBot - what is your name?</h2>';				  
 
 	var output = document.getElementById('output');
 	output.innerHTML = question;												
 
 }
 
+function userResponse(){
+    var userRsp = document.getElementById("response").value;
+    console.log(Response);
+    resp.innerHTML = userRsp;
+    
+    if (Response!=null)
+    {
+    	 userRsp ="Hello " + Response + "! How are you today?";
+         document.getElementById("resp").innerHTML=userRsp;
+    }
+              
 
-function bot() { 
+}
+
+/*function bot() { 
     var response = document.getElementById("response").value;
     console.log(response);
 
-    if (menu_option == 0) {
+    if (menu_option === 0) {
     output.innerHTML = '<h1>hello ' + response + '</h1>';
     document.getElementById("response").value = "";  
     question = '<h1>how old are you?</h1>';		
     setTimeout(timedQuestion, 2000);
     }
 
-    else if (menu_option == 1) {
+    else if (menu_option === 1) {
     output.innerHTML = '<h1>That means you were born in ' + (2017 - response) + '</h1>';
     document.getElementById("response").value = "";   
     question = '<h1>where are you from?</h1>';					      	
@@ -31,7 +45,7 @@ function bot() {
 
 function timedQuestion() {
     output.innerHTML = question;
-}
+}*/
 
 //push enter key (using jquery)
 //$(document).keypress(function(e) {
@@ -40,7 +54,7 @@ function timedQuestion() {
    // menu_option++;																	
 //});
 
-document.getElementById("response")
+/*document.getElementById("response")
     .addEventListener("keyup", function(event) {
     event.preventDefault();
     if (event.keyCode == 13) {
@@ -48,4 +62,4 @@ document.getElementById("response")
         bot();
         menu_option++;
     }
-});
+});*/
