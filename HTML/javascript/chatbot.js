a04b9f76b54cc3cdc6ba6ad30483cb58e155fdff
catherinegracekane <catherinekane@gmail.com>
2017-04-22 00:11:57 +0100

    bootstrap added, robot added, formatting changed on this page and will be required to be changed throughout site, JS bot has an error with features and a menu is still to be added

--- HTML/javascript/chatbot.js
+++ HTML/javascript/chatbot.js
@@ -1,39 +1,56 @@
+var menu_option = 0;
+
 function myFunction() {
     
-    document.getElementById("demo").innerHTML = "Test JS File";
+    //document.getElementById("demo").outerHTML = "Test JS File";
     
-    var menu_option = 0;
-	var question = '<h2>Hello, my name is TrackBot - what is your name?</h2>';				  
-
+	var question = ['Hello, my name is TrackBot - what is your name?', 'Hey, what did ya say you are called?', 'Hola, que tal?'];				  
+	var questionRand = question[Math.floor(Math.random() * question.length)];
 	var output = document.getElementById('output');
-	output.innerHTML = question;												
+	output.innerHTML = questionRand;												
 
 }
 //response is id of input box 
 //resp is id of paragraph under submit button
 //respond is name out div, same as output is name of div
+
+/* I want to generate random responses where possible and track here e.g. 
+var greetArray[ 'Hi there ', 'Hello ', 'Greetings ', 'Wassup '];
+var greetRand = greetArray[Math.floor(Math.random() * greetArray.length)];
+*/
+
 function userResponse(){
+	
+	//variables
+	var greetArray = [ 'Hi there ', 'Hello ', 'Greetings ', 'Wassup '];
+	var greetRand = greetArray[Math.floor(Math.random() * greetArray.length)];
     var userRsp = document.getElementById("response").value;
+	var output = document.getElementById('output');
+	    
+    //counter to track responses
+    menu_option++;
     
+    //using this to test the inputs I'm getting within my code
+    console.log(userRsp);
+    console.log(menu_option);
+    console.log(greetRand);
+    console.log(output);
+    //resp.innerHTML = userRsp;
     
-    //var respond = document.getElementById('respond');
-    	console.log(userRsp);
-    resp.innerHTML = userRsp;
-    
-    if (response!=null)
+    if (menu_option < 2)
     {
-    	 output.innerHTML = 'Hello ' + userRsp + '! How can I help you today?';
-         document.getElementById("resp").innerHTML=userRsp;
+    	 output.innerHTML = 'Test ' + greetRand + userRsp + '! How can I help you today?';
+         //document.getElementById("resp").innerHTML=userRsp;
          document.getElementById("response").value = " ";         
     }
     
-    if (userRsp === 'features')
+    if (userRsp == 'features' && menu_option > 2)
     {
     	 output.innerHTML = 'You want help with ' + userRsp + '! What is the issue?';
          document.getElementById("resp").innerHTML=userRsp;
+         document.getElementById("response").value = " ";   
 	}    	 
             
-
 }
 
 /*function bot() { 
